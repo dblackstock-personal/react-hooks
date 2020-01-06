fedd0ad619e990e8f298f5906b886e324b2a42a9
dblackstock-personal <davey.blackstock@gmail.com>
2020-01-06 10:27:42 +0000

    complete example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [faveThings, setFaveThings] = useState(
+    [
+      {
+        name: "Raindrops on Roses",
+        price: 20,
+        order: 1
+      },
+      {
+        name: "Whiskers on Kittens",
+        price: 10,
+        order: 2
+      },
+      {
+        name: "Bright Copper Kettles",
+        price: 54,
+        order: 3
+      },
+      {
+        name: "Warm Woolen Mittens",
+        price: 12,
+        order: 4
+      },
+      {
+        name: "White Geese That Fly With The Moon on Their Wings",
+        price: 10,
+        order: 5
+      }
+    ]
+  );
+
+  const increasePrice = (index) => {
+    let changedThings = [...faveThings];
+    let item = {
+      ...changedThings[index],
+      price: changedThings[index].price + 1
+    };
+    changedThings[index] = item;
+
+    setFaveThings(
+      changedThings
+    )
+  }
+
+  // this one is Telmo's description
+  const changeName = (index) => {
+    const newArray = [...faveThings];
+    newArray[0].name = "Schnitzel With Noodles"
+    setFaveThings(newArray)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Things That Are My Favourite</h1>
+      <h2>Brought to you by React Hooks</h2>
+      <ul>
+        {faveThings.map((object, index) => {
+          return <li key={index}>{object.order} {object.name}, Price: £{object.price}</li>
+        })}
+      </ul>
+      <button id="btn" onClick={() => increasePrice(2)}>Increase Kettle Price</button>
+      <button id="btn" onClick={() => changeName(0)}>Change Raindrops to Schnitzel</button>
     </div>
   );
 }
